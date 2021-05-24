aa21a5cc7582321dc803a93c926d1bc28f9be191
jfloresmaquera <jfloresm96@gmail.com>
2021-05-24 14:48:09 -0300

    Arreglado iteracion 4

--- src/main/java/ApiClima/ProveedorClimaAccuWeatherApi.java
+++ src/main/java/ApiClima/ProveedorClimaAccuWeatherApi.java
@@ -1,15 +1,38 @@
 package ApiClima;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ProveedorClimaAccuWeatherApi implements ProveedorClima {
 
-    AccuWeatherAPI miApi = new AccuWeatherAPI();
+    private AccuWeatherAPI miApi;
+    private Map<String, Object> ultimasRespuestas;
+    private Duration periodoDeValidez;
+    private LocalDateTime proximaExpiracion;
+
+    public ProveedorClimaAccuWeatherApi() {
+        miApi = new AccuWeatherAPI();
+        ultimasRespuestas = new HashMap<String, Object>();
+        periodoDeValidez = Duration.ofMinutes(150);
+        proximaExpiracion = LocalDateTime.now();
+    }
 
     public Map<String, Object> climaSegunAPISeleccionada() {
-        //miApi.controlarCantidadMaximaDeConsultas();
-        return miApi.getWeather("Buenos Aires, Argentina").get(0);
+        if(this.expiro()){
+            ultimasRespuestas = miApi.getWeather("Buenos Aires, Argentina").get(0);
+            proximaExpiracion = this.proximaExpiracion();
+        }
+        return ultimasRespuestas;
+    }
+
+    public LocalDateTime proximaExpiracion(){
+        return LocalDateTime.now().plus(this.periodoDeValidez);
+    }
+
+    private boolean expiro() {
+        return proximaExpiracion.isBefore(LocalDateTime.now());
     }
 
     public double getTemperatura(){
